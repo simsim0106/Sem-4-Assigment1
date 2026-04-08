4a89d0e75bec0f5a7b8a86e9d78aa57cc2a1aff1
Simran Chaudhary <simranchaudhary781@gmail.com>
2026-04-08 14:27:37 +0530

    main.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-import ast
-
 import pandas as pd
 import numpy as np
 
-df=pd.read_csv('International_T20_Data.csv')
-df.head()
+# Load dataset
+df = pd.read_csv('International_T20_Data.csv')
 
-#for renaming columns
-df.columns = df.columns.str.replace(".", "_")
+# Replace '.' with '_'
+df.columns = df.columns.str.replace('.', '_')
 
-df=df.rename(columns={
+# Rename columns
+df.rename(columns={
     'meta_data_version':'data_version',
     'meta_created':'created_date',
     'meta_revision':'revision',
@@ -33,67 +32,9 @@ df=df.rename(columns={
     'info_outcome_eliminator':'eliminator',
     'info_outcome_bowl_out':'bowl_out',
     'info_neutral_venue':'neutral_venue'
-})
+}, inplace=True)
+
+# Save file
 df.to_csv('International_T20_Data.csv', index=False)
+
 print("Changes have been permanently saved to the CSV file.")
-
-#The top three venues which hosted the greatest number of matches
-top_venues = df['venue'].value_counts().head(3)
-print(top_venues)
-
-
-#the pair of cricket teams who played the most number of T20 matches against each other.
-df['teams'] = df['teams'].apply(lambda x: x.strip("[]").replace("'", "").split(", ") if isinstance(x, str) else x)
-df['team_pair'] = df['teams'].apply(lambda x: tuple(sorted(x)))
-most_matches = df['team_pair'].value_counts().head(1)
-print(most_matches)
-df['teams'] = df['teams'].apply(lambda x: x.strip("[]").replace("'", "").split(", ") if isinstance(x,str) else x)
-matches_played = df['teams'].explode().value_counts()
-matches_won = df['match_winner'].value_counts()
-win_percentage = (matches_won / matches_played) * 100
-top_teams = win_percentage.sort_values(ascending=False).head(5)
-print(top_teams)
-
-
-#score card
-df['innings'] = df['innings'].apply(ast.literal_eval)
-def get_scorecard(innings):
-
-    batsman_runs = {}
-    bowler_wickets = {}
-
-    for inning in innings:
-        inning_data = list(inning.values())[0]
-
-        for delivery in inning_data['deliveries']:
-
-            ball = list(delivery.values())[0]
-
-            batsman = ball['batsman']
-            bowler = ball['bowler']
-            runs = ball['runs']['batsman']
-
-            batsman_runs[batsman] = batsman_runs.get(batsman, 0) + runs
-
-            if 'wicket' in ball:
-                bowler_wickets[bowler] = bowler_wickets.get(bowler, 0) + 1
-
-    batsman_df = pd.DataFrame(
-        batsman_runs.items(),
-        columns=['Batsman','Runs']
-    ).sort_values(by='Runs', ascending=False).head(4)
-
-    bowler_df = pd.DataFrame(
-        bowler_wickets.items(),
-        columns=['Bowler','Wickets']
-    ).sort_values(by='Wickets', ascending=False).head(4)
-
-    return batsman_df, bowler_df
-
-scorecard1, scorecard2 = get_scorecard(df['innings'][0])
-
-print("Top 4 Batsmen")
-print(scorecard1)
-
-print("\nTop 4 Bowlers")
-print(scorecard2)
